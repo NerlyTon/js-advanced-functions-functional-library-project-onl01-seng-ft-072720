249fbd29dc4db1ec0f807a91317f4cab3f1a9b9b
NerlyTon <nerlyton@gmail.com>
2020-11-10 17:31:46 -0600

    commit tree 29b848d1cfb53154282860515bd0f78e44a72457

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      Object.values(collection).forEach(x => callback(x))
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArray = []
+      Object.values(collection).forEach(x => newArray.push(callback(x)))
+      return newArray
     },
 
     reduce: function() {
